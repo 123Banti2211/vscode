85472cb501404b71e2d78d2e747c0d2fc0044206
123Banti2211 <97242916+123Banti2211@users.noreply.github.com>
2022-02-02 15:00:21 +0530

    delete exe

---
+++ patternbox.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int i,j,n;
+    printf("ENTER THE LINE NUMBER:");
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+        for(j=1;j<=n;j++)
+        {
+            if((i==1||i==n || j==1||j==n) || (i==j))
+            {
+                printf("* ");
+            }
+            else
+            {
+                printf("  ");
+            }
+        }
+        printf("\n");
+    }
+    
+}
