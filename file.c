2cbecd3f9bca1f85850d7133ac93d958f91e683c
123Banti2211 <97242916+123Banti2211@users.noreply.github.com>
2022-02-26 21:50:12 +0530

    no

--- file.c
+++ file.c
@@ -1,8 +1,32 @@
+// #include<stdio.h>
+// void main()
+// {
+//     int i;
+//     FILE * fp;
+//     // char s[]="Hellow students";
+//     char s[100];
+//     fp=fopen("f1.txt","w");
+//     if(fp==NULL)
+//     {
+//         printf("File cannot open");
+//         exit(1);
+//     }
+//     printf("enter a string");
+//     gets(s);
+//     for(i=0;i<strlen(s);i++)
+//     {
+//         fputc(s[i],fp);
+//     }
+//     fclose(fp);
+// }
+
+
 #include<stdio.h>
-#include<conio.h>
 void main()
 {
-    char name[50];
-    int i,num,marks,age;
-    printf("ENTER A NUMBER");
+    FILE * fp;
+    if(fp==NULL)
+    {
+        printf("File not found");
+    }
 }
